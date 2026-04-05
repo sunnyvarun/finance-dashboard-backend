8008d20eaa7e39e299e9e307665976320817a206
varun sunny <varunsunchu59@gmail.com>
2026-04-06 05:06:47 +0530

    fix: update swagger server URL

--- config/swagger.js
+++ config/swagger.js
@@ -19,7 +19,7 @@ const options = {
         },
     servers: [
       {
-        url: 'http://localhost:3000'
+        url: 'http://finance-dashboard-backend-sqbs.onrender.com'
       }
     ]
   },
